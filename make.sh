6046baf2f49fe6d1c49d61f8cb723b2dbc508c23
lxvm <lorenzo@vanmunoz.com>
2022-12-23 11:36:59 -0500

    automate environment setup

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+julia --project=. -e 'import Pkg; Pkg.instantiate()'
 julia --project=. --threads 1 code/make_plots.jl
 
 mv -t report fields.png scaling.png densities.png params.png densities_conv_direct.png fields_conv_direct.png densities_conv_fast.png fields_conv_fast.png
