2feeb5f288f196faed89149a6e5729eb00b26f5a
lxvm <lorenzo@vanmunoz.com>
2023-03-24 16:01:03 -0400

    oops: add develop path in julia environment

--- make.sh
+++ make.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-julia --project=. -e 'import Pkg; Pkg.instantiate()'
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+METABIEPATH=$SCRIPTPATH/MetaBIE
+
+julia --project=. -e "import Pkg; Pkg.develop(path=\"$METABIEPATH\"); Pkg.instantiate()"
 julia --project=. --threads 1 code/make_plots.jl
 
 mv -t report fields.png scaling.png densities.png params.png densities_conv_direct.png fields_conv_direct.png densities_conv_fast.png fields_conv_fast.png
